d64375f61d255ecee492e7aeb36cdb70dbfec176
surishubham <shubhamsuri@lambdatest.com>
2020-09-14 11:05:06 +0530

    push

---
+++ docs/doc4.md
@@ -0,0 +1,13 @@
+---
+id: doc4
+title: Heading 4
+sidebar_label: Heading 4
+slug: /document4
+---
+
+# heading4
+abc
+
+## markdown
+
+abcd
--- sidebars.js
+++ sidebars.js
@@ -2,5 +2,6 @@ module.exports = {
   someSidebar: {
     Level1: ['doc1', 'doc2', 'doc3'],
     Level2: ['mdx'],
+    Level3: ['doc4'],
   },
 };
